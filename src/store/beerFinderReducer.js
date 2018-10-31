8e55884c0aa4170ca8006206a4da60d748b6a8d6
Shane EC Davis <secd0183@yahoo.com>
2018-10-30 20:37:27 -0500

    loading apollo to help with graphql

---
+++ src/store/beerFinderReducer.js
@@ -0,0 +1,17 @@
+// rate beer end point: https://api.ratebeer.com/v1/api/graphql
+
+// sample query using graphQL
+// query{
+//    beerSearch(query:"floyds"){items{
+//     name,
+//     id,
+//     abv,
+//     description,
+//     ibu,
+//     imageUrl
+//   }
+  
+//   }
+//   }
+  
+  
