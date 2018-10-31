65873ba80ff660faca4f04698f81431c5baf5001
Shane EC Davis <secd0183@yahoo.com>
2018-10-31 18:50:15 -0500

    playing with env variables.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
@@ -8,12 +10,33 @@ import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
 import history from './history'
 
+/*
+'content-type: application/json' \
+-H 'accept: application/json' \
+-H 'x-api-key: <YOUR_API_KEY>'
+
+
+It provides 3 headers:
+ 1. content-type with a value of 'application/json' to indicate that the body uses the JSON format.
+ 2. accept with a value of 'application/json' to indicate we're expecting JSON format in response.
+ 3. x-api-key the value for which you'll need to supply the API Key you've received.
+*/
+
+const client = new ApolloClient({
+  uri: 'https://api.ratebeer.com/v1/api/graphql',
+  headers: {'content-type':'application/json',
+  'accept': 'application/json',
+'x-api-key':''}
+})
+
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
+  </ApolloProvider>,
 
   document.getElementById('root')
 )
