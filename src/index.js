864c809ccd568bd52a81ae8099c95c09f43cd6ad
Shane EC Davis <shaneecdavis7@gmail.com>
2018-11-01 10:15:04 -0500

    have apollo and graphql hooked in

--- src/index.js
+++ src/index.js
@@ -28,11 +28,21 @@ const topBeersQuery = gql`
     }
   }
 `
+const ratebeerapi = process.env.REACT_APP_RATEBEERAPI
+const client = new ApolloClient({
+  uri: 'https://api.ratebeer.com/v1/api/graphql',
+  headers: {
+    'content-type': 'application/json',
+    accept: 'application/json',
+    'x-api-key': ratebeerapi
+  }
+})
+
 client
   .query({
-    query: testQuery
+    query: topBeersQuery
   })
-  .then(res => console.log(res))
+  .then(res => console.log(res, 'test query for single beer-----------909'))
 /*
 'content-type: application/json' \
 -H 'accept: application/json' \
@@ -45,15 +55,6 @@ It provides 3 headers:
  3. x-api-key the value for which you'll need to supply the API Key you've received.
 */
 
-const ratebeerapi = process.env.REACT_APP_RATEBEERAPI
-const client = new ApolloClient({
-  uri: 'https://api.ratebeer.com/v1/api/graphql',
-  headers: {
-    'content-type': 'application/json',
-    accept: 'application/json',
-    'x-api-key': ratebeerapi
-  }
-})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
