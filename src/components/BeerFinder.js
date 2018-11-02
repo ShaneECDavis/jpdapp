1e1477eb4317c543ad8a6edd6d988f1a760aae1f
Shane EC Davis <shaneecdavis7@gmail.com>
2018-11-01 22:56:39 -0500

    working on beerfinder comp

--- src/components/BeerFinder.js
+++ src/components/BeerFinder.js
@@ -1,4 +1,5 @@
 import React , {Component} from 'react'
+import {Card,TextField,FormGroup, Grid } from '@material-ui/core'
 
 
 
@@ -6,11 +7,90 @@ class BeerFinder extends Component {
 
   render (){
 
-    return (
-      <div>
-        BeerFinder
-        Inster beer api
-      </div>
+      return (
+      <Fragment>
+        <Grid
+          container
+          justify="center"
+          direction="column-reverse"
+          alignItems="center"
+        >
+          <Grid>
+            <Card raised style={{ width: '99vw' }}>
+              <form onSubmit={this.handleSubmit.bind(this)} style={styles.form}>
+                <FormGroup>
+                  <TextField
+                    name="diameter"
+                    variant="outlined"
+                    style={styles.textField}
+                    value={this.state.diameter}
+                    onChange={this.handleChange}
+                    label="Diameter"
+                    placeholder="diameter"
+                  />
+                  <TextField
+                    name="flutes"
+                    variant="outlined"
+                    style={styles.textField}
+                    value={this.state.flutes}
+                    onChange={this.handleChange}
+                    label="flutes"
+                    placeholder="flutes"
+                  />
+                  <TextField
+                    name="ipt"
+                    variant="outlined"
+                    style={styles.textField}
+                    value={this.state.ipt}
+                    onChange={this.handleChange}
+                    label="IPT(C/L)"
+                    placeholder="IPT(C/L)"
+                  />
+
+                  <TextField
+                    name="sfm"
+                    variant="outlined"
+                    style={styles.textField}
+                    value={this.state.sfm}
+                    onChange={this.handleChange}
+                    label="SFM"
+                    placeholder="SFM"
+                  />
+
+                  <TextField
+                    name="rpm"
+                    disabled
+                    variant="outlined"
+                    style={styles.changed}
+                    value={this.props.rpm}
+                    label="RPM"
+                    placeholder="RPM"
+                  />
+
+                  <TextField
+                    name="ipm"
+                    disabled
+                    variant="outlined"
+                    value={this.props.ipm}
+                    style={styles.changed}
+                    label="IPM"
+                    placeholder="IPM"
+                  />
+
+                  <Grid container direction="column" alignItems="center">
+                    <Grid>
+                      <Button type="submit" style={styles.button}>
+                        TADB
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </FormGroup>
+              </form>
+            </Card>
+          </Grid>
+        </Grid>
+      </Fragment>
+    )
     )
   }
 }
@@ -24,3 +104,32 @@ const mapDispatch = dispatch =>({
 })
 
 export default BeerFinder
+
+
+
+const styles = {
+  form: {
+    justifiedContent: 'spaceBetween',
+    width: '95vw',
+    height: '100%',
+    alignItems: 'center'
+  },
+  textField: {
+    width: '85vw',
+    margin: 20,
+    paddingRight: 5
+  },
+  changed: {
+    width: '85vw',
+    margin: 20,
+    paddingRight: 5,
+    background: '#b8ffb7',
+    color: 'black'
+  },
+  button: {
+    width: '85vw',
+    background: '#3247ff',
+    margin: 20,
+    color: 'white'
+  }
+}
