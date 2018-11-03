8bb044e2dc9b468e930877549aafaf6338d7375c
Shane EC Davis <shaneecdavis7@gmail.com>
2018-11-03 00:00:41 -0500

    add query components

--- src/components/BeerFinder.js
+++ src/components/BeerFinder.js
@@ -1,107 +1,138 @@
 import React , {Component} from 'react'
 import {Card,TextField,FormGroup, Grid } from '@material-ui/core'
+import {gql} from 'graphql-tag'
+import { Query } from 'react-apollo'
 
 
 
 class BeerFinder extends Component {
 
   render (){
-
-      return (
-      <Fragment>
-        <Grid
-          container
-          justify="center"
-          direction="column-reverse"
-          alignItems="center"
-        >
-          <Grid>
-            <Card raised style={{ width: '99vw' }}>
-              <form onSubmit={this.handleSubmit.bind(this)} style={styles.form}>
-                <FormGroup>
-                  <TextField
-                    name="diameter"
-                    variant="outlined"
-                    style={styles.textField}
-                    value={this.state.diameter}
-                    onChange={this.handleChange}
-                    label="Diameter"
-                    placeholder="diameter"
-                  />
-                  <TextField
-                    name="flutes"
-                    variant="outlined"
-                    style={styles.textField}
-                    value={this.state.flutes}
-                    onChange={this.handleChange}
-                    label="flutes"
-                    placeholder="flutes"
-                  />
-                  <TextField
-                    name="ipt"
-                    variant="outlined"
-                    style={styles.textField}
-                    value={this.state.ipt}
-                    onChange={this.handleChange}
-                    label="IPT(C/L)"
-                    placeholder="IPT(C/L)"
-                  />
-
-                  <TextField
-                    name="sfm"
-                    variant="outlined"
-                    style={styles.textField}
-                    value={this.state.sfm}
-                    onChange={this.handleChange}
-                    label="SFM"
-                    placeholder="SFM"
-                  />
-
-                  <TextField
-                    name="rpm"
-                    disabled
-                    variant="outlined"
-                    style={styles.changed}
-                    value={this.props.rpm}
-                    label="RPM"
-                    placeholder="RPM"
-                  />
-
-                  <TextField
-                    name="ipm"
-                    disabled
-                    variant="outlined"
-                    value={this.props.ipm}
-                    style={styles.changed}
-                    label="IPM"
-                    placeholder="IPM"
-                  />
-
-                  <Grid container direction="column" alignItems="center">
-                    <Grid>
-                      <Button type="submit" style={styles.button}>
-                        TADB
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </FormGroup>
-              </form>
-            </Card>
-          </Grid>
-        </Grid>
-      </Fragment>
-    )
-    )
+    const TOP_BEERS = gql`
+      {
+topBeers{
+  items{
+    name
+    imageUrl
+  	ibu
   }
 }
+  
+}
+    `
+    return (
+     <Query query={TOP_BEERS}>
+        {({loading, error, data})=>{
+          if(loading) return 'loading ...'
+          if(error) return `Error! ${error.message}`
 
-const mapState = state =>({
-  topbeers: state.topbeers
-})
+          return (
+            <div>
+              {data.topBeers.items.map(beer =>{
+                <Card> <div>{beer.name} </div> </Card>
+              })}
+            </div> 
+          )
+        }}
+       </Query>
 
-const mapDispatch = dispatch =>({
-  searchBeer: (beerSearch) => dispatch(searchBeer(beerSearch))
-})
+    )
+
+      // return (
+      // <Fragment>
+      //   <Grid
+      //     container
+      //     justify="center"
+      //     direction="column-reverse"
+      //     alignItems="center"
+      //   >
+      //     <Grid>
+      //       <Card raised style={{ width: '99vw' }}>
+      //         <form onSubmit={this.handleSubmit.bind(this)} style={styles.form}>
+      //           <FormGroup>
+      //             <TextField
+      //               name="diameter"
+      //               variant="outlined"
+      //               style={styles.textField}
+      //               value={this.state.diameter}
+      //               onChange={this.handleChange}
+      //               label="Diameter"
+      //               placeholder="diameter"
+      //             />
+      //             <TextField
+      //               name="flutes"
+      //               variant="outlined"
+      //               style={styles.textField}
+      //               value={this.state.flutes}
+      //               onChange={this.handleChange}
+      //               label="flutes"
+      //               placeholder="flutes"
+      //             />
+      //             <TextField
+      //               name="ipt"
+      //               variant="outlined"
+      //               style={styles.textField}
+      //               value={this.state.ipt}
+      //               onChange={this.handleChange}
+      //               label="IPT(C/L)"
+      //               placeholder="IPT(C/L)"
+      //             />
+
+      //             <TextField
+      //               name="sfm"
+      //               variant="outlined"
+      //               style={styles.textField}
+      //               value={this.state.sfm}
+      //               onChange={this.handleChange}
+      //               label="SFM"
+      //               placeholder="SFM"
+      //             />
+
+      //             <TextField
+      //               name="rpm"
+      //               disabled
+      //               variant="outlined"
+      //               style={styles.changed}
+      //               value={this.props.rpm}
+      //               label="RPM"
+      //               placeholder="RPM"
+      //             />
+
+      //             <TextField
+      //               name="ipm"
+      //               disabled
+      //               variant="outlined"
+      //               value={this.props.ipm}
+      //               style={styles.changed}
+      //               label="IPM"
+      //               placeholder="IPM"
+      //             />
+
+      //             <Grid container direction="column" alignItems="center">
+      //               <Grid>
+      //                 <Button type="submit" style={styles.button}>
+      //                   TADB
+      //                 </Button>
+      //               </Grid>
+      //             </Grid>
+      //           </FormGroup>
+      //         </form>
+      //       </Card>
+      //     </Grid>
+      //   </Grid>
+      // </Fragment>
+  }
+    
+  }
+
+
+// const mapState = state =>({
+//   topbeers: state.topbeers
+// })
+
+// const mapDispatch = dispatch =>({
+//   searchBeer: (beerSearch) => dispatch(searchBeer(beerSearch))
+// })
 
 export default BeerFinder
 
