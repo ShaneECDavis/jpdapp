4e1801963cc6c6d65586901ea9720a545e6c751e
Shane EC Davis <shaneecdavis7@gmail.com>
2018-11-14 21:18:43 -0600

    before deployment

--- src/components/BeerFinder.js
+++ src/components/BeerFinder.js
@@ -158,34 +158,6 @@ class BeerFinder extends Component {
   }
 }
 
-// const styles = theme => ({
-//   card: {
-//     maxWidth: 400
-//   },
-//   media: {
-//     height: 0,
-//     paddingTop: '56.25%' // 16:9
-//   },
-//   actions: {
-//     display: 'flex'
-//   },
-//   expand: {
-//     transform: 'rotate(0deg)',
-//     transition: theme.transitions.create('transform', {
-//       duration: theme.transitions.duration.shortest
-//     }),
-//     marginLeft: 'auto',
-//     [theme.breakpoints.up('sm')]: {
-//       marginRight: -8
-//     }
-//   },
-//   expandOpen: {
-//     transform: 'rotate(180deg)'
-//   },
-//   avatar: {
-//     backgroundColor: red[500]
-//   }
-// })
 
 export default BeerFinder
 
